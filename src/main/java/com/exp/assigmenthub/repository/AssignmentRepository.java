125f50530398262ca4be0fccf25517d9ccf5e997
Авдеев Матвей Анатольевич <maavdeev@edu.hse.ru>
2026-07-07 14:28:34 +0300

    feat: assignment service is ready

--- src/main/java/com/exp/assigmenthub/repository/AssignmentRepository.java
+++ src/main/java/com/exp/assigmenthub/repository/AssignmentRepository.java
@@ -1,10 +1,7 @@
 package com.exp.assigmenthub.repository;
 
 import com.exp.assigmenthub.model.Assignment;
-import com.exp.assigmenthub.model.Submission;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 import java.util.UUID;
 
 public interface AssignmentRepository extends JpaRepository<Assignment, UUID> {
